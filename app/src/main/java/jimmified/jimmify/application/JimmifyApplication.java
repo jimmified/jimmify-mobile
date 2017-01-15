7f4aca4836b5ea39833dcb667c92365d8f5c4bd3
sugarmanz <zucker.jeremiah@gmail.com>
2017-01-15 18:00:55 -0500

    Point to jimmified.com/api.

--- app/src/main/java/jimmified/jimmify/application/JimmifyApplication.java
+++ app/src/main/java/jimmified/jimmify/application/JimmifyApplication.java
@@ -22,7 +22,7 @@ public class JimmifyApplication extends Application {
 
     // Api for retrofit
     private static JimmifyAPI jimmifyAPI;
-    private static String jimmifyURL = "http://shibboleth.student.rit.edu/";
+    private static String jimmifyURL = "http://www.jimmified.com/api/";
 
     public void onCreate() {
         super.onCreate();
